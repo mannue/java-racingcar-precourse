aa77a76cc3dc53ea8f8b164d2018e5ee07c741e0
mannue <sppe12@gmail.com>
2022-04-21 00:49:00 +0900

    refactor($Energy): Energy 테스트 중복 제거
    파라미터 테스트를 통해 중복 제거함

--- src/test/java/racingcar/EnergyTest.java
+++ src/test/java/racingcar/EnergyTest.java
@@ -1,7 +1,11 @@
 package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -9,16 +13,16 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class EnergyTest {
 
     @DisplayName("Energy 는 int 값을 전달 받고 VO 객체이다.")
-    @Test
-    public void validTest() {
-        assertThat(new Energy(0)).isEqualTo(new Energy(0));
-        assertThat(new Energy(0)).isNotEqualTo(new Energy(9));
+    @ParameterizedTest
+    @CsvSource(value = {"0:0:True", "0:9:False"}, delimiter = ':')
+    public void validTest(final int value, final int target, final boolean expectedResult) {
+        assertThat(Objects.equals(new Energy(value),new Energy(target))).isEqualTo(expectedResult);
     }
 
     @DisplayName("Energy 는 0~9 까지의 값만 갖는다.")
-    @Test
-    public void invalidTest() {
-        assertThatThrownBy(() -> new Energy(-1)).isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() ->  new Energy(10)).isInstanceOf(IllegalArgumentException.class);
+    @ParameterizedTest
+    @ValueSource(ints = {-1,10})
+    public void invalidTest(final int item) {
+        assertThatThrownBy(() -> new Energy(item)).isInstanceOf(IllegalArgumentException.class);
     }
 }
