8031b79d018f99ea6748d210baecb58c5b52df41
mannue <sppe12@gmail.com>
2022-04-22 01:39:08 +0900

    refactor($EnergyGenerator): EnergyGenerator create 메소드 테스트 입력값 수정

--- src/test/java/racingcar/EnergyGeneratorTest.java
+++ src/test/java/racingcar/EnergyGeneratorTest.java
@@ -2,15 +2,19 @@ package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class EnergyGeneratorTest {
     @DisplayName("EnergyGenerator 는 생성 범위를 가지며 사용자 입력 숫자만큼 Energy 를 생성한다.")
-    @Test
-    public void createTest() {
+    @ParameterizedTest
+    @ValueSource(ints = {1,9})
+    public void createTest(final int number) {
         EnergyGenerator energyGenerator = new EnergyGenerator(1, 9);
-        Energy[] createEnergy = energyGenerator.create(1);
-        assertThat(createEnergy.length).isEqualTo(1);
+        Energy[] createEnergy = energyGenerator.create(number);
+        assertThat(createEnergy.length).isEqualTo(number);
     }
+
 }
