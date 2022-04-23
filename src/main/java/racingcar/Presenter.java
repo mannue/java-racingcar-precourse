ff537dd71e2c443d3fe90fd71cbe20b1f204923c
mannue <sppe12@gmail.com>
2022-04-23 15:44:29 +0900

    refactor($Presenter): 입력 값이 잘못된 경우 테스트 추가 및 반환값 타입 변경
    - 입력이 잘못된 경우 ERROR 를 출력한다.
    - 반환 값 타입을 Optional 로 변경

--- src/main/java/racingcar/Presenter.java
+++ src/main/java/racingcar/Presenter.java
@@ -1,11 +1,26 @@
 package racingcar;
 
+import java.util.*;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Presenter {
     private static final String INPUT_DELIMITER = ",";
 
-    public static String[] inputCarNames() {
-        return readLine().split(INPUT_DELIMITER);
+    public static Optional<Name[]> inputCarNames() {
+        try {
+            return Optional.of(validationInput(readLine().split(INPUT_DELIMITER)));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        return Optional.empty();
+    }
+
+    private static Name[] validationInput(final String[] names) {
+        Set<Name> nameSet = new HashSet<>();
+        for (String name : names) {
+            nameSet.add(new Name((name)));
+        }
+        return nameSet.toArray(new Name[0]);
     }
 }
--- src/test/java/racingcar/PresenterTest.java
+++ src/test/java/racingcar/PresenterTest.java
@@ -1,6 +1,8 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -9,18 +11,14 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
+import java.util.Optional;
 import java.util.stream.Stream;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
-public class PresenterTest {
-
-    @DisplayName("사용자가 name 입력시 Array 반환")
-    @Test
-    public void inputTest() {
-        String[] names = Presenter.inputCarNames();
-        assertThat(names).isNotNull();
-    }
+public class PresenterTest extends NsTest {
+    private static final String ERROR_MESSAGE = "[ERROR]";
 
     @DisplayName("사용자가 쉼표를 기준으로 입력한 갯수만큼 Array 를 반환한다.")
     @ParameterizedTest
@@ -28,8 +26,9 @@ public class PresenterTest {
     public void sameInputSizeWithListSize(final String mockInput, final int expectedLength) {
         try (MockedStatic<Console> consoleMockedStatic = Mockito.mockStatic(Console.class)) {
             consoleMockedStatic.when(Console::readLine).thenReturn(mockInput);
-            String[] names = Presenter.inputCarNames();
-            assertThat(names.length).isEqualTo(expectedLength);
+            Optional<Name[]> isNames = Presenter.inputCarNames();
+            assertThat(isNames.isPresent()).isTrue();
+            assertThat(isNames.get().length).isEqualTo(expectedLength);
         }
     }
 
@@ -38,5 +37,21 @@ public class PresenterTest {
                 Arguments.of("pobi,woni,jun", 3),
                 Arguments.of("pobi",1)
         );
+    }
+
+    @DisplayName("사용자가 입력을 잘못하면 ERROR 메시지를 출력한다.")
+    @Test
+    public void invalidInputTest() {
+        try (MockedStatic<Console> consoleMockedStatic = Mockito.mockStatic(Console.class)) {
+            consoleMockedStatic.when(Console::readLine).thenReturn("");
+            Presenter.inputCarNames();
+            assertSimpleTest(() -> Assertions.assertThat(output()).contains(ERROR_MESSAGE));
+        }
+    }
+
+
+    @Override
+    protected void runMain() {
+        Presenter.inputCarNames();
     }
 }
